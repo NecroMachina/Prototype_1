f6fa5d5773d70b481e7c16e97dcb75b39b3c90a1
Robin Fros <317370@student.mboutrecht.nl>
2019-11-07 12:45:57 +0100

    no message

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // private variables
+    [SerializeField]
+    private float speed = 15.0f;
+    [SerializeField]
+    private float turnSpeed = 20.0f;
+    [SerializeField]
+    private float horizontalInput;
+    [SerializeField]
+    private float forwardInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // We'll move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
